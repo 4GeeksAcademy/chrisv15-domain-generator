248545c579289d98e608beef8bd007ae78b1f7ec
chrisv15 <128597332+chrisv15@users.noreply.github.com>
2023-05-30 20:33:04 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,63 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = [
+    "yo",
+    "tu",
+    "el",
+    "nosotros",
+    "vosotros",
+    "ellos",
+    "elle",
+    "yo",
+    "tu",
+    "el",
+    "mi"
+  ];
+  var adj = [
+    "gran",
+    "gigante",
+    "pequeño",
+    "alto",
+    "diminuto",
+    "mediano",
+    "inteligente",
+    "dorado",
+    "feo",
+    "delgado",
+    "brillante"
+  ];
+  var noun = [
+    "jogger",
+    "racoon",
+    "pollo",
+    "tenedor",
+    "mando",
+    "computador",
+    "aritos",
+    "estuche",
+    "sombrilla",
+    "celular",
+    "sol"
+  ];
+  var extension = [
+    ".es",
+    ".com",
+    ".co",
+    ".de",
+    ".org",
+    ".mx",
+    "fr",
+    "pt",
+    "eg",
+    ".us",
+    ".gb"
+  ];
+  let dominioGenerado =
+    pronoun[Math.floor(Math.random() * 10)] +
+    adj[Math.floor(Math.random() * 10)] +
+    noun[Math.floor(Math.random() * 10)] +
+    extension[Math.floor(Math.random() * 10)];
+
+  console.log(dominioGenerado);
 };
